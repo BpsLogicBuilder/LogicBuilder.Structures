395b080e43094f5e8effec9484cae20678884ec4
Blaise Taylor <btaylor@bPolicySolutions.com>
2026-03-13 09:02:34 -0400

    Add project files.

---
+++ LogicBuilder.Structures/ExpressionDescriptors/FilterLambdaDescriptor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LogicBuilder.Expressions.Utils.ExpressionDescriptors
+{
+    public class FilterLambdaDescriptor : IExpressionDescriptor
+    {
+		public FilterLambdaDescriptor()
+		{
+		}
+
+		public FilterLambdaDescriptor(IExpressionDescriptor filterBody, Type sourceElementType, string parameterName)
+		{
+			FilterBody = filterBody;
+			SourceElementType = sourceElementType;
+			ParameterName = parameterName;
+		}
+
+		public IExpressionDescriptor FilterBody { get; set; }
+		public Type SourceElementType { get; set; }
+		public string ParameterName { get; set; }
+    }
+}
