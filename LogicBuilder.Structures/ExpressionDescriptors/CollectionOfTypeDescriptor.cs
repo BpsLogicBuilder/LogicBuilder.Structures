9e5076079555da506bcd4dac8f7b271e7e93bcd5
Blaise Taylor <btaylor@bPolicySolutions.com>
2026-03-24 14:46:38 -0400

    AB#97: Add CollectionOfTypeDescriptor and serialization test
    Introduced the CollectionOfTypeDescriptor class with Operand and Type properties, inheriting from DescriptorBase. Added a unit test to verify correct serialization and deserialization of this descriptor. Also cleaned up CollectionCastDescriptor.cs by removing an unused using directive and applying file-scoped namespace formatting.

--- LogicBuilder.Structures/ExpressionDescriptors/CollectionOfTypeDescriptor.cs
+++ LogicBuilder.Structures/ExpressionDescriptors/CollectionOfTypeDescriptor.cs
@@ -1,6 +1,6 @@
 ﻿namespace LogicBuilder.Expressions.Utils.ExpressionDescriptors
 {
-    public class CollectionCastDescriptor(DescriptorBase operand, string type) : DescriptorBase
+    public class CollectionOfTypeDescriptor(DescriptorBase operand, string type) : DescriptorBase
     {
         public DescriptorBase Operand { get; set; } = operand;
         public string Type { get; set; } = type;
